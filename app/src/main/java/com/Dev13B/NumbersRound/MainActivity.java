4f76e63f151412b9478e20622b252a0876f9b4d9
Nick Penney <41111180+13BDev@users.noreply.github.com>
2019-06-25 01:39:56 +0100

    First upload.

---
+++ app/src/main/java/com/Dev13B/NumbersRound/MainActivity.java
@@ -0,0 +1,24 @@
+package com.Dev13B.NumbersRound;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void introToast(){
+
+        Toast.makeText(getApplicationContext(), "Hello, Android!", Toast.LENGTH_LONG).show();
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        introToast();
+
+    }
+}
