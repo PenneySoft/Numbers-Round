f9a2ec85c2665a0bd04ad76f06d680b5c660283c
Nick Penney <41111180+13BDev@users.noreply.github.com>
2019-06-26 18:18:24 +0100

    4th upload.
    Pics and textviews. Branched.
    Background position and size fixed.
    All tiles and text workign eventually, keep emulating.
    Making picker screen buttons.
    Buttons basically assembled.
    Writing the code for the number picking screen first.
    
    Game will start now, just make a method to clear screen and show game screen. Don't forget clock hand hide and go button show. We only want the random num tiles shown, make all orig tiles blue as this will also be used in our replay method, which will pick new numbers but of the same size. Need a button that goes back to picker screen.

--- app/src/main/java/com/Dev13B/NumbersRound/MainActivity.java
+++ app/src/main/java/com/Dev13B/NumbersRound/MainActivity.java
@@ -10,10 +10,58 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-        // Make all mystery tiles 50% transparent
+        // For tracking how many random tiles the user has chosen
+    int smallNum, bigNum, totalNum;
+    int maxBig = 4;
+
+
+        // When user taps a tile. Get the tile row to determine if its big.
+    public void randomTileClicked(View view){
+
+        Boolean bigFlag = false;
+
+        ViewGroup bigVG = (ViewGroup)findViewById(R.id.bigBacking);
+        if (view.getParent() == bigVG) {
+                bigFlag = true;
+        }
+
+            // If clicked, reverse Alpha and score
+        if (view.getAlpha() == (1.0f)){
+            view.setAlpha(0.5f);
+            if (bigFlag == true){
+                bigNum--;
+            } else {
+                smallNum--;
+            }
+        } else { // choosing, make alpha 1 and click counter
+            view.setAlpha(1.0f);
+            if (bigFlag == true){
+                bigNum++;
+            } else {
+                smallNum++;
+            }
+        } // end of clicking/unclicking (if)
+
+
+
+
+        totalNum = smallNum + bigNum;
+        if (totalNum > 5){
+            Toast.makeText(getApplicationContext(), "Starting!\nBig: " + Integer.toString(bigNum) + "\nSmall: " + Integer.toString(smallNum), Toast.LENGTH_LONG).show();
+        }
+
+
+    } // end of randomTileClicked
+
+
+
+
+        // Make all mystery tiles 50% transparent, the pickerScreen visible, and reset tile counts to 0
     public void refreshPicker(View view){
 
-        // All change smallNum, bigNum and totalNum to 0 ?
+        smallNum = 0;
+        bigNum = 0;
+        totalNum = 0;
 
         ViewGroup bigVG = (ViewGroup)findViewById(R.id.pickerScreen);
         bigVG.setVisibility(View.VISIBLE);
@@ -22,29 +70,25 @@ public class MainActivity extends AppCompatActivity {
         for (int i=0; i<bigVG.getChildCount(); i++) {
             bigVG.getChildAt(i).setAlpha(0.5f);
         }
-
         bigVG = (ViewGroup)findViewById(R.id.smallRow0);
         for (int i=0; i<bigVG.getChildCount(); i++) {
             bigVG.getChildAt(i).setAlpha(0.5f);
         }
-
         bigVG = (ViewGroup)findViewById(R.id.smallRow1);
         for (int i=0; i<bigVG.getChildCount(); i++) {
             bigVG.getChildAt(i).setAlpha(0.5f);
         }
-
         bigVG = (ViewGroup)findViewById(R.id.smallRow2);
         for (int i=0; i<bigVG.getChildCount(); i++) {
             bigVG.getChildAt(i).setAlpha(0.5f);
         }
-
         bigVG = (ViewGroup)findViewById(R.id.smallRow3);
         for (int i=0; i<bigVG.getChildCount(); i++) {
             bigVG.getChildAt(i).setAlpha(0.5f);
         }
+    } // end of refreshPicker
 
 
-    }
 
 
 
@@ -52,6 +96,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            // Starts the game with pickerScreen Visible and all buttons Alpha 0.5f
+        ImageView nullIV = (ImageView)findViewById(R.id.big0);
+        refreshPicker(nullIV);
 
 
 
