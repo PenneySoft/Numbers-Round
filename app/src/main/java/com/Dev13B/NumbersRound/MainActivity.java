a048bd10e36447b16a473cb5a8fdb9d6fc1ef442
Nick Penney <41111180+13BDev@users.noreply.github.com>
2019-06-26 17:01:08 +0100

    4th upload.
    Pics and textviews. Branched.
    Background position and size fixed.
    
    All tiles and text workign eventually, keep emulating.
    
    Making picker screen buttons.
    
    Buttons basically assembled.
    
    Writing the code for the number picking screen first.

--- app/src/main/java/com/Dev13B/NumbersRound/MainActivity.java
+++ app/src/main/java/com/Dev13B/NumbersRound/MainActivity.java
@@ -4,18 +4,48 @@ import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    /*
-    public void introToast(){
+        // Make all mystery tiles 50% transparent
+    public void refreshPicker(View view){
 
-        Toast.makeText(getApplicationContext(), "Hello, Android!", Toast.LENGTH_LONG).show();
+        // All change smallNum, bigNum and totalNum to 0 ?
+
+        ViewGroup bigVG = (ViewGroup)findViewById(R.id.pickerScreen);
+        bigVG.setVisibility(View.VISIBLE);
+
+        bigVG = (ViewGroup)findViewById(R.id.bigBacking);
+        for (int i=0; i<bigVG.getChildCount(); i++) {
+            bigVG.getChildAt(i).setAlpha(0.5f);
+        }
+
+        bigVG = (ViewGroup)findViewById(R.id.smallRow0);
+        for (int i=0; i<bigVG.getChildCount(); i++) {
+            bigVG.getChildAt(i).setAlpha(0.5f);
+        }
+
+        bigVG = (ViewGroup)findViewById(R.id.smallRow1);
+        for (int i=0; i<bigVG.getChildCount(); i++) {
+            bigVG.getChildAt(i).setAlpha(0.5f);
+        }
+
+        bigVG = (ViewGroup)findViewById(R.id.smallRow2);
+        for (int i=0; i<bigVG.getChildCount(); i++) {
+            bigVG.getChildAt(i).setAlpha(0.5f);
+        }
+
+        bigVG = (ViewGroup)findViewById(R.id.smallRow3);
+        for (int i=0; i<bigVG.getChildCount(); i++) {
+            bigVG.getChildAt(i).setAlpha(0.5f);
+        }
+
 
     }
-    */
+
 
 
     @Override
@@ -23,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // introToast();
+
 
     }
 
